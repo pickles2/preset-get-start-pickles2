d468f973fbd7e081c56d176af67ff5a12d5dfb47
Tomoya Koyanagi <tomk79@gmail.com>
2019-07-10 13:35:18 +0900

    Theme "gui_sample" を更新 ＆ デフォルト化

--- px-files/config.php
+++ px-files/config.php
@@ -297,7 +297,7 @@ return call_user_func( function(){
 			'cookie_theme_switch'=>'THEME',
 			'path_theme_collection'=>'./px-files/themes/',
 			'attr_bowl_name_by'=>'data-contents-area',
-			'default_theme_id'=>'pickles2',
+			'default_theme_id' => 'gui_sample',
 		)).')' ,
 
 		// Apache互換のSSIの記述を解決する
